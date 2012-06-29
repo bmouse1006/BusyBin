36478ad4eb1305adc27f3f9c4c517800db6960ed
Jin Jin <bmouse1006@msn.com>
2012-06-29 11:49:34 +0800

    Using box2d

---
+++ BusyBin/BusyBin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BusyBin
+//
+//  Created by Jin Jin on 12-6-29.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
